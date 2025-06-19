f558b76ba1e1d3fcc7d681c682110a4b598d18dc
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2025-06-19 15:50:55 -0300

    feat: começo da criação do component Card

---
+++ src/components/card/index.tsx
@@ -0,0 +1,21 @@
+import './style.css';
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="card-image">
+        <span>imagem</span>
+        <button className="card-button">VER</button>
+      </div>
+
+      <div className="card-info">
+        <p><strong>Nome:</strong> x</p>
+        <p><strong>Data:</strong> X</p>
+        <p><strong>Tecnologias:</strong> A, B, C, D</p>
+        <p><strong>Descrição:</strong> Blá, Blá, Blá</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
