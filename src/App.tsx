aadd443f58a077e50a52a9920922f2dc1db267b1
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2025-06-20 12:56:30 -0300

    feat: Adiciona projeto - Regiões

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,21 @@ function App() {
       />
       <Card
         name="Navegadores"  // 15 de Fevereiro de 2023
-        technologies={["HTML",]} 
+        technologies={["HTML", "CSS",]} 
         description="Este foi meu primeiro contato com diversos fatores importantes para a programação.
         Foi meu primeiro uso de imagens, entendendo o elemento <img>; meu primeiro contato com links e redirecionamento;
         e principalmente, meu primeiro contato com CSS, mexendo basicamente com cores, fontes e um pouco de alinhamento"
         image="navegadores.png" 
         link='/projects/navegadores/index.html'
+      />
+      <Card
+        name="Regiões"  // 23 de Fevereiro de 2023
+        technologies={["HTML", "CSS",]} 
+        description="Foi com esse pequeno site que aprendi a programar listas em HTML,
+        compreendendo na teoria e na prática os elementos <ul> e <li>.
+        Também ajudou em Geografia..."
+        image="regioes.png" 
+        link='/projects/listas/regioes.html'
       />
     </div>
   );
