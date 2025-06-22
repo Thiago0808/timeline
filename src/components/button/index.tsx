edc5a37007d75e9ae9bf5d002432c4554ab08237
Thiago0808 <thiago.mo08@aluno.ifsc.edu.br>
2025-06-21 23:13:07 -0300

    feat: Adiciona ícone de lupa no Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 interface ButtonProps {
   text: string;
@@ -11,6 +13,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ text, href, target = "_blank", rel = "noopener noreferrer" }) => {
   return (
     <a className="custom-button" href={href} target={target} rel={rel}>
+      <FontAwesomeIcon icon={faSearch} style={{ marginRight: '8px' }} />
       {text}
     </a>
   );
